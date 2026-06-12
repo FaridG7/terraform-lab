21f41e099a889823d1644b78a65ea7792624f80b
Farid <goharifarid7@gmail.com>
2026-06-13 02:38:55 +0330

    starting over, add cloud init disk

--- main.tf
+++ main.tf
@@ -8,9 +8,9 @@ terraform {
   required_version = "~> 1.15.6"
 }
 
-variable "vm_hostname" {
-  description = "Hostname for the virtual machine"
-  type        = string
+variable "vm" {
+  description = "Data used for the vm initialization"
+  type        = map(string)
 }
 
 variable "ssh_public_key_path" {
@@ -18,130 +18,31 @@ variable "ssh_public_key_path" {
   type        = string
 }
 
-provider "libvirt" {
-  uri = "qemu:///system"
-}
-
-resource "libvirt_volume" "ubuntu_base" {
-  name = "ubuntu-24.04.qcow2"
-  pool = "default"
-
-  target = {
-    format = {
-      type = "qcow2"
-    }
-  }
-
-  create = {
-    content = {
-      # url = "https://cloud-images.ubuntu.com/noble/20260518/noble-server-cloudimg-amd64.img"
-      url = "./iso/noble-server-cloudimg-amd64.img"
-    }
-  }
-}
-
-resource "libvirt_volume" "root_disk" {
-  name     = "${var.vm_hostname}-root.qcow2"
-  pool     = "default"
-  capacity = 21474836480 # 20 GB in bytes
-  target = {
-    format = {
-      type = "qcow2"
-    }
-  }
-  backing_store = {
-    path = libvirt_volume.ubuntu_base.path
-    format = {
-      type = "qcow2"
-    }
-  }
-}
-
 resource "libvirt_cloudinit_disk" "init" {
-  name = "${var.vm_hostname}-cloudinit.iso"
-
-  user_data = templatefile("${path.module}/cloud_init.tftpl", {
-    hostname       = var.vm_hostname
+  name = "vm-init"
+  user_data = templatefile("${path.module}/user-data.tftpl", {
+    hostname       = var.vm.hostname
     ssh_public_key = trimspace(file(var.ssh_public_key_path))
   })
 
-  meta_data = templatefile("${path.module}/meta_data.tftpl", {
-    hostname    = var.vm_hostname
-    instance_id = var.vm_hostname
+  meta_data = templatefile("${path.module}/meta-data.tftpl", {
+    hostname    = var.vm.hostname
+    instance_id = var.vm.hostname
   })
 }
 
 resource "libvirt_volume" "cloudinit" {
-  name = "${var.vm_hostname}-cloudinit"
+  name = "vm-cloudinit"
   pool = "default"
+  target = {
+    format = {
+      type = "raw"
+    }
+  }
+
   create = {
     content = {
       url = libvirt_cloudinit_disk.init.path
     }
   }
-}
-
-resource "libvirt_domain" "vm" {
-  name        = var.vm_hostname
-  memory      = 4096
-  memory_unit = "MiB"
-  vcpu        = 4
-  type        = "kvm"
-
-  os = {
-    type = "hvm"
-  }
-
-  cpu = {
-    mode = "host-passthrough"
-  }
-
-  features = {
-    acpi = true
-  }
-
-  devices = {
-    disks = [
-      {
-        target = {
-          dev = "vda"
-          bus = "virtio"
-        }
-
-        source = {
-          volume = {
-            volume = libvirt_volume.root_disk.name
-            pool   = libvirt_volume.root_disk.pool
-          }
-        }
-      },
-      {
-        device = "cdrom"
-        target = { dev = "sda", bus = "sata" }
-        source = {
-          file = {
-            file = libvirt_volume.cloudinit.path
-          }
-        }
-      }
-    ]
-
-    interfaces = [
-      {
-        model = {
-          type = "virtio"
-        }
-        source = {
-          network = {
-            network = "default"
-          }
-        }
-      }
-    ]
-  }
-}
-
-output "vm_ip" {
-  description = "IP address assigned to the VM"
-  value       = libvirt_domain.vm.devices.interfaces[0].ip
 }
